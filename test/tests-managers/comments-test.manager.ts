2e52d468c66a4888fd0137c65f903d8c51e8c987
belij2111 <belijvalerij2111@gmail.com>
2025-03-04 13:02:39 +0300

    feat(test):Add tests to create comment for the specified post

---
+++ test/tests-managers/comments-test.manager.ts
@@ -0,0 +1,123 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { CommentCreateModel } from '../../src/features/bloggers-platform/comments/api/models/input/create-comment.input.model';
+import { CommentViewModel } from '../../src/features/bloggers-platform/comments/api/models/view/comment.view.model';
+import { paginationParams } from '../models/base/pagination.model';
+import { Paginator } from '../../src/core/models/pagination.base.model';
+import { createValidCommentModel } from '../models/bloggers-platform/comment.input.model';
+import { LikeInputModel } from '../../src/features/bloggers-platform/likes/api/models/input/like.input.model';
+
+export class CommentsTestManager {
+  constructor(private readonly app: INestApplication) {}
+
+  async createComment(
+    accessToken: string,
+    postId: string,
+    createdModel: CommentCreateModel,
+    statusCode: number = HttpStatus.CREATED,
+  ) {
+    const response = await request(this.app.getHttpServer())
+      .post(`/posts/${postId}/comments`)
+      .auth(accessToken, { type: 'bearer' })
+      .send(createdModel)
+      .expect(statusCode);
+    return response.body;
+  }
+
+  expectCorrectModel(
+    createdModel: CommentCreateModel,
+    responseModel: CommentViewModel,
+  ) {
+    expect(createdModel.content).toBe(responseModel.content);
+  }
+
+  async createComments(
+    accessToken: string,
+    postId: string,
+    count: number,
+    statusCode: number = HttpStatus.CREATED,
+  ) {
+    const comments: CommentViewModel[] = [];
+    for (let i = 1; i <= count; i++) {
+      const response = await request(this.app.getHttpServer())
+        .post(`/posts/${postId}/comments`)
+        .auth(accessToken, { type: 'bearer' })
+        .send(createValidCommentModel(i))
+        .expect(statusCode);
+      comments.push(response.body);
+    }
+    return comments;
+  }
+
+  async getCommentsWithPaging(
+    postId: string,
+    statusCode: number = HttpStatus.OK,
+  ) {
+    const { pageNumber, pageSize, sortBy, sortDirection } = paginationParams;
+    return request(this.app.getHttpServer())
+      .get(`/posts/${postId}/comments`)
+      .query({
+        pageNumber,
+        pageSize,
+        sortBy,
+        sortDirection,
+      })
+      .expect(statusCode);
+  }
+
+  expectCorrectPagination(
+    createModels: CommentCreateModel[],
+    responseModels: Paginator<CommentViewModel[]>,
+  ) {
+    expect(responseModels.items.length).toBe(createModels.length);
+    expect(responseModels.totalCount).toBe(createModels.length);
+    expect(responseModels.items).toEqual(createModels);
+    expect(responseModels.pagesCount).toBe(1);
+    expect(responseModels.page).toBe(1);
+    expect(responseModels.pageSize).toBe(10);
+  }
+
+  async getCommentById(id: string, statusCode: number = HttpStatus.OK) {
+    const response = await request(this.app.getHttpServer())
+      .get('/comments/' + id)
+      .expect(statusCode);
+    return response.body;
+  }
+
+  async update(
+    accessToken: string,
+    commentId: string,
+    createdModel: CommentCreateModel,
+    statusCode: number = HttpStatus.NO_CONTENT,
+  ) {
+    await request(this.app.getHttpServer())
+      .put(`/comments/${commentId}`)
+      .auth(accessToken, { type: 'bearer' })
+      .send(createdModel)
+      .expect(statusCode);
+  }
+
+  async delete(
+    accessToken: string,
+    commentId: string,
+    statusCode: number = HttpStatus.NO_CONTENT,
+  ) {
+    await request(this.app.getHttpServer())
+      .delete(`/comments/${commentId}`)
+      .auth(accessToken, { type: 'bearer' })
+      .expect(statusCode);
+  }
+
+  async updateLikeStatus(
+    accessToken: string,
+    commentId: string,
+    createdModel: LikeInputModel | string,
+    statusCode: number = HttpStatus.NO_CONTENT,
+  ) {
+    await request(this.app.getHttpServer())
+      .put(`/comments/${commentId}/like-status`)
+      .auth(accessToken, { type: 'bearer' })
+      .send(createdModel)
+      .expect(statusCode);
+  }
+}
---
+++ test/tests-managers/core-test.manager.ts
@@ -0,0 +1,34 @@
+import { UserCreateModel } from '../../src/features/user-accounts/users/api/models/input/create-user.input.model';
+import { createValidUserModel } from '../models/user-accounts/user.input.model';
+import { UsersTestManager } from './users-test.manager';
+import { AuthTestManager } from './auth-test.manager';
+
+export class CoreTestManager {
+  constructor(
+    private readonly usersTestManager: UsersTestManager,
+    private readonly authTestManager: AuthTestManager,
+  ) {}
+
+  async loginUser(count: number = 1) {
+    const validUserModel: UserCreateModel = createValidUserModel(count);
+    await this.usersTestManager.createUser(validUserModel);
+    return await this.authTestManager.loginUser(validUserModel);
+  }
+
+  async loginSeveralUsers(count: number = 1) {
+    const validUserModel: UserCreateModel = createValidUserModel(count);
+    await this.usersTestManager.createUser(validUserModel);
+    const loginResults = await this.authTestManager.loginWithRateLimit(
+      validUserModel,
+      count,
+    );
+    return loginResults
+      .filter((el): el is { accessToken: string; refreshToken: string } => {
+        return (
+          (el as { accessToken: string; refreshToken: string }).refreshToken !==
+          undefined
+        );
+      })
+      .map((el) => el.refreshToken);
+  }
+}
