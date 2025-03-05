312fcff1633e3cb578ca9c24994ff598717f64cf
belij2111 <belijvalerij2111@gmail.com>
2025-03-05 17:16:59 +0300

    feat: Update the method to clean blogs collection

--- src/features/testing/infrastructure/testing.repository.ts
+++ src/features/testing/infrastructure/testing.repository.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import {
-  Blog,
-  BlogModelType,
-} from '../../bloggers-platform/blogs/domain/blog.entity';
-import {
   Post,
   PostModelType,
 } from '../../bloggers-platform/posts/domain/post.entity';
@@ -22,7 +18,6 @@ import { DataSource } from 'typeorm';
 @Injectable()
 export class TestingRepository {
   constructor(
-    @InjectModel(Blog.name) private BlogModel: BlogModelType,
     @InjectModel(Post.name) private PostModel: PostModelType,
     @InjectModel(Comment.name) private CommentModel: CommentModelType,
     @InjectModel(Like.name) private LikeModel: LikeModelType,
@@ -31,13 +26,15 @@ export class TestingRepository {
 
   async deleteAllData() {
     await Promise.all([
-      this.BlogModel.deleteMany(),
       this.PostModel.deleteMany(),
       this.CommentModel.deleteMany(),
       this.LikeModel.deleteMany(),
       this.dataSource.query(
-        `TRUNCATE TABLE public."users", 
-        public."securityDevices" RESTART IDENTITY`,
+        `TRUNCATE TABLE 
+        public."users", 
+        public."securityDevices",
+        public."blogs" 
+         RESTART IDENTITY`,
       ),
     ]);
   }
