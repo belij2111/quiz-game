9edc98fb63621ccce99be4ac0c7b41a48c9649f7
belij2111 <belijvalerij2111@gmail.com>
2025-03-28 17:46:02 +0300

    feat: Update the methods to clean comments and  likesForComments collections

--- src/features/testing/infrastructure/testing.repository.ts
+++ src/features/testing/infrastructure/testing.repository.ts
@@ -1,41 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import {
-  Post,
-  PostModelType,
-} from '../../bloggers-platform/posts/domain/post.entity';
-import {
-  Comment,
-  CommentModelType,
-} from '../../bloggers-platform/comments/domain/comment.entity';
-import {
-  Like,
-  LikeModelType,
-} from '../../bloggers-platform/likes/domain/like.entity';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 
 @Injectable()
 export class TestingRepository {
-  constructor(
-    @InjectModel(Post.name) private PostModel: PostModelType,
-    @InjectModel(Comment.name) private CommentModel: CommentModelType,
-    @InjectModel(Like.name) private LikeModel: LikeModelType,
-    @InjectDataSource() private dataSource: DataSource,
-  ) {}
+  constructor(@InjectDataSource() private dataSource: DataSource) {}
 
   async deleteAllData() {
     await Promise.all([
-      this.PostModel.deleteMany(),
-      this.CommentModel.deleteMany(),
-      this.LikeModel.deleteMany(),
       this.dataSource.query(
         `TRUNCATE TABLE 
         public."users", 
         public."securityDevices",
         public."blogs",
         public."posts",
-        public."likesForPosts"
+        public."likesForPosts",
+        public."comments"
+        public."likesForComments"
          RESTART IDENTITY`,
       ),
     ]);
