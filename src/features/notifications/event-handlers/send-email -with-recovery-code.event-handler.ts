dd12f546894405687476ab7d9d1c49efa42a05ec
belij2111 <belijvalerij2111@gmail.com>
2025-04-18 20:57:55 +0300

    refactor: Rename the password recovery event via email confirmation

--- src/features/notifications/event-handlers/send-email -with-recovery-code.event-handler.ts
+++ src/features/notifications/event-handlers/send-email -with-recovery-code.event-handler.ts
@@ -1,18 +1,18 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
-import { UserRegistrationConfirmationEvent } from '../../user-accounts/auth/application/events/user-registration-confirmation.event';
+import { UserPasswordRecoveryEvent } from '../../user-accounts/auth/application/events/user-password-recovery.event';
 import { MailerService } from '@nestjs-modules/mailer';
 import { EmailTemplateService } from '../email-template.service';
 
-@EventsHandler(UserRegistrationConfirmationEvent)
+@EventsHandler(UserPasswordRecoveryEvent)
 export class SendEmailWithRecoveryCodeEventHandler
-  implements IEventHandler<UserRegistrationConfirmationEvent>
+  implements IEventHandler<UserPasswordRecoveryEvent>
 {
   constructor(
     private readonly mailerService: MailerService,
     private readonly emailTemplateService: EmailTemplateService,
   ) {}
 
-  async handle(event: UserRegistrationConfirmationEvent) {
+  async handle(event: UserPasswordRecoveryEvent) {
     const htmlContent = this.emailTemplateService.passwordRecoveryEmail(
       event.recoveryCode,
     );
--- src/features/user-accounts/auth/application/events/user-password-recovery.event.ts
+++ src/features/user-accounts/auth/application/events/user-password-recovery.event.ts
@@ -1,4 +1,4 @@
-export class UserRegistrationConfirmationEvent {
+export class UserPasswordRecoveryEvent {
   constructor(
     public email: string,
     public recoveryCode: string,
