092e8aec880434c24283e0d5cd937d61e63f6a6f
belij2111 <belijvalerij2111@gmail.com>
2025-04-26 21:14:48 +0300

    refactor: Update the types for the base entity

--- src/core/entities/base.entity.ts
+++ src/core/entities/base.entity.ts
@@ -7,11 +7,10 @@ import {
 export abstract class BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   public id: string;
-  @CreateDateColumn({
-    type: 'timestamp with time zone',
-    default: () => 'CURRENT_TIMESTAMP',
-  })
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
   public createdAt: Date;
-  @DeleteDateColumn()
-  public deletedAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamp with time zone', nullable: true })
+  public deletedAt: Date | null;
 }
