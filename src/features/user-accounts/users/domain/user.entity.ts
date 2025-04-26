17ea19e75783032cecb15ab47ebe63d2220fbd73
belij2111 <belijvalerij2111@gmail.com>
2025-04-26 21:19:19 +0300

    refactor: Update an entity for create user

--- src/features/user-accounts/users/domain/user.entity.ts
+++ src/features/user-accounts/users/domain/user.entity.ts
@@ -1,10 +1,27 @@
-export class User {
-  id: string;
-  login: string;
-  password: string;
-  email: string;
-  createdAt: Date;
-  confirmationCode: string;
-  expirationDate: Date;
-  isConfirmed: boolean;
+import { Column, Entity, OneToOne } from 'typeorm';
+import { BaseEntity } from '../../../../core/entities/base.entity';
+import { EmailConfirmation } from './email-confirmation.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ type: 'varchar', unique: true, collation: 'C' })
+  public login: string;
+
+  @Column({ type: 'varchar', unique: true })
+  public password: string;
+
+  @Column({ type: 'varchar', unique: true, collation: 'C' })
+  public email: string;
+
+  @Column({ type: 'boolean', default: false })
+  public isConfirmed: boolean = false;
+
+  @Column({ type: 'uuid', unique: true, nullable: true })
+  public recoveryCode: string | null;
+
+  @Column({ type: Date, nullable: true })
+  public expirationRecoveryCode: Date | null;
+
+  @OneToOne(() => EmailConfirmation, (e) => e.user)
+  public emailConfirmation: EmailConfirmation;
 }
