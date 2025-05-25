233cde6a5e420a089b4a507e97bab6d00594727c
belij2111 <belijvalerij2111@gmail.com>
2025-05-25 11:31:40 +0300

    fix: Update the type for the code

--- src/features/notifications/email-template.service.ts
+++ src/features/notifications/email-template.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EmailTemplateService {
-  registrationEmail(code: string): string {
+  registrationEmail(code: string | null): string {
     return `<h1>Thanks for your registration</h1>
  <p>To finish registration please follow the link below:
      <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
