7ee88ab78a348a562f94c5aa02c07b059c692819
belij2111 <belijvalerij2111@gmail.com>
2025-06-13 20:07:26 +0300

    chore: Update deployment settings

--- src/core/config/env-file-paths.ts
+++ src/core/config/env-file-paths.ts
@@ -1,10 +1,12 @@
+import { join } from 'path';
+
 if (!process.env.NODE_ENV) {
   throw new Error('NODE_ENV environment is missing');
 }
 
 export const envFilePaths = [
   process.env.ENV_FILE_PATH?.trim() || '',
-  `src/env/.env.${process.env.NODE_ENV}.local`,
-  `src/env/.env.${process.env.NODE_ENV}`,
-  'src/env/.env.production',
+  join(process.cwd(), `src/env/.env.${process.env.NODE_ENV}.local`),
+  join(process.cwd(), `src/env/.env.${process.env.NODE_ENV}`),
+  join(process.cwd(), `src/env/.env.production`),
 ];
