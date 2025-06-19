2b24340afdc26a4562ab1f4a428ca3f1bfc74b7b
belij2111 <belijvalerij2111@gmail.com>
2025-06-19 14:54:27 +0300

    feat: Add the quiz-game module

--- src/features/quiz-game/quiz-game.module.ts
+++ src/features/quiz-game/quiz-game.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserAccountsModule } from '../user-accounts/user-accounts.module';
+import { PairGamePublicController } from './pair-game/api/pair-game.public-controller';
+
+const controllers = [PairGamePublicController];
 
 @Module({
-  controllers: [],
+  imports: [TypeOrmModule.forFeature([]), UserAccountsModule],
+  controllers: [...controllers],
   providers: [],
 })
 export class QuizGameModule {}
