68a464245becce9f99e29784f7ff025e4f3382c4
belij2111 <belijvalerij2111@gmail.com>
2025-06-21 13:01:02 +0300

    feat(db): Add a question entity

---
+++ migrations/1750499649189-add-question.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddQuestion1750499649189 implements MigrationInterface {
+  name = 'AddQuestion1750499649189';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE "questions"
+                                 (
+                                     "created_at"      TIMESTAMP WITH TIME ZONE      NOT NULL DEFAULT now(),
+                                     "updated_at"      TIMESTAMP WITH TIME ZONE      NOT NULL DEFAULT now(),
+                                     "deleted_at"      TIMESTAMP WITH TIME ZONE,
+                                     "id"              uuid                          NOT NULL DEFAULT uuid_generate_v4(),
+                                     "body"            character varying COLLATE "C" NOT NULL,
+                                     "correct_answers" jsonb                         NOT NULL DEFAULT '[]',
+                                     "published"       boolean                       NOT NULL DEFAULT false,
+                                     CONSTRAINT "PK_08a6d4b0f49ff300bf3a0ca60ac" PRIMARY KEY ("id")
+                                 )`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "questions"`);
+  }
+}
---
+++ src/features/quiz-game/questions/domain/questions.entity.ts
@@ -0,0 +1,28 @@
+import { BaseWithUuidIdEntity } from '../../../../core/entities/base-with-uuid-id.entity';
+import { Column, Entity } from 'typeorm';
+import { CreateQuestionDto } from '../dto/create-question.dto';
+
+@Entity()
+export class Question extends BaseWithUuidIdEntity {
+  @Column({ type: 'varchar', collation: 'C' })
+  body: string;
+
+  @Column({ type: 'jsonb', default: [] })
+  correctAnswers: string[];
+
+  @Column({ type: 'boolean', default: false })
+  published: boolean;
+
+  static create(dto: CreateQuestionDto) {
+    const question = new this();
+    question.body = dto.body;
+    question.correctAnswers = dto.correctAnswers;
+    question.published = false;
+    return question;
+  }
+
+  update(data: Partial<Question>) {
+    Object.assign(this, data);
+    this.updatedAt = new Date();
+  }
+}
