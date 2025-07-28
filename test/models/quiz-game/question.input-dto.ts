891107b92945bc1934120a8ff7c567f26e61af01
belij2111 <belijvalerij2111@gmail.com>
2025-07-28 21:00:25 +0300

    refactor(test): Update the list of questions to make it easier to use them during checks

--- test/models/quiz-game/question.input-dto.ts
+++ test/models/quiz-game/question.input-dto.ts
@@ -6,20 +6,20 @@ export const QUESTIONS_PULL = [
     correctAnswers: ['4', 'four'],
   },
   {
-    body: 'Question: What color mixes with yellow to make green?',
-    correctAnswers: ['blue', 'cyan'],
+    body: 'Question: What is 2 + 2?',
+    correctAnswers: ['4', 'four'],
   },
   {
-    body: 'Question: Which continent is the largest?',
-    correctAnswers: ['Asia', 'Eurasia'],
+    body: 'Question: What is 2 + 2?',
+    correctAnswers: ['4', 'four'],
   },
   {
-    body: "Question: Which element is represented by the symbol 'H'?",
-    correctAnswers: ['hydrogen', 'H'],
+    body: 'Question: What is 2 + 2?',
+    correctAnswers: ['4', 'four'],
   },
   {
-    body: 'Question: What year is considered the founding year of Rome?',
-    correctAnswers: ['753 BC', '753', 'seven hundred fifty-three BC'],
+    body: 'Question: What is 2 + 2?',
+    correctAnswers: ['4', 'four'],
   },
 ];
 
